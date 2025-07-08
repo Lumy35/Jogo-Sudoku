676ffcfe56425b2c641cd944234cac624aed98bf
LuanSilva <lc.sillva1404@gmail.com>
2025-07-08 20:07:04 -0300

    Versão inicial do jogo criada.

---
+++ src/main/model/GameStatusEnum.java
@@ -0,0 +1,19 @@
+package main.model;
+
+public enum GameStatusEnum {
+
+    NON_STARTED("não iniciado"),
+    INCOMPLETE("imcompleto"),
+    COMPLETE("completo");
+
+    private String label;
+
+    GameStatusEnum(final String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+}
